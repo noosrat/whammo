3538cfeb60fc5b847708af723aba9f6683b86a19
noosrath <noosrat.hossain@gmail.com>
2018-11-22 14:36:31 +0200

    attempt at creating a utilities class that will have the bulk of the code

---
+++ app/src/main/java/com/example/noosrat/budgettracker/SpentUtilities.java
@@ -0,0 +1,123 @@
+package com.example.noosrat.budgettracker;
+
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.Telephony;
+import android.support.v4.content.ContextCompat;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class SpentUtilities {
+
+    public boolean isBundledSms(String sms){
+
+        int pos = sms.indexOf(":");
+        String bank = sms.substring(0, pos);
+
+        if (bank.equals("FNB")){
+            int separator = sms.indexOf(";");
+
+            if (separator>-1)
+                    return true;
+        }
+
+
+        return false;
+
+    }
+
+    public String[] smsCleaner(String message){
+
+        int pos = message.indexOf(")"); //end of FNB:-)
+        message = message.substring(pos+2);
+
+        String[] messages_list = new String[0];
+        if (message.charAt(0) != 'R') {
+
+            message = message.substring(message.indexOf(":")+2);
+            messages_list = message.split(";");
+            }
+
+            if (messages_list[messages_list.length-1].matches("Avail*")){
+                messages_list = Arrays.copyOfRange(messages_list, 0,messages_list.length-2);
+            }
+
+
+
+            return messages_list;
+
+    }
+    
+    public ArrayList<String> getSMSes(String[] banks, Date date, ContentResolver cr, Uri uri){
+
+
+        ArrayList<String> lstSms = new ArrayList<String>();
+
+        Date monthStart = new Date();
+        monthStart.setDate(1);
+        monthStart.setTime(0);
+
+        long millis = monthStart.getTime();
+
+
+        Cursor c = cr.query(uri, // Official CONTENT_URI from docs
+                new String[] { Telephony.Sms.Inbox.BODY,  Telephony.Sms.Inbox.DATE}, // Select body text
+                "date > ? AND body LIKE ?",
+                new String[] {"" + millis, banks[0] + "%"},
+                Telephony.Sms.Inbox.DEFAULT_SORT_ORDER); // Default sort order
+
+        int totalSMS = c.getCount();
+
+        if (c.moveToFirst()) {
+
+            for (int i = 0; i < totalSMS; i++) {
+                lstSms.add(c.getString(0));
+
+                c.moveToNext();
+
+
+            }
+        } else {
+            throw new RuntimeException("You have no SMS in Inbox");
+        }
+            c.close();
+
+
+
+            return lstSms;
+    }
+
+    public float calculateExpenses(ArrayList<SMS> transactionList){
+
+        float sum = 0;
+
+        for (int i=0; i<transactionList.size(); i++){
+            sum = sum + transactionList.get(i).getNumberAmount();
+        }
+
+        return 0;
+    }
+
+    public ArrayList<SMS> SMStoTransaction(ArrayList<String> smsList){
+
+        ArrayList<SMS> lstSms = new ArrayList<>();
+
+
+        for (int i = 0; i < smsList.size(); i++) {
+            SMS sms = new SMS(smsList.get(i));
+            if ((sms.getTransactionType() != 3) && (sms.getTransactionType() != 1)) {
+                lstSms.add(sms);
+
+            }
+        }
+
+        return lstSms;
+    }
+
+
+}

1abda9fa9ac2bfd968420b10b1cf0ee587e4043e
noosrath <noosrat.hossain@gmail.com>
2018-11-24 18:32:53 +0200

    request sms permissions

--- app/src/main/java/com/example/noosrat/budgettracker/MainActivity.java
+++ app/src/main/java/com/example/noosrat/budgettracker/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.noosrat.budgettracker;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,8 +24,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onContinueClicked(View view) {
-        Intent i = new Intent(MainActivity.this, FeedActivity.class);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.READ_SMS},
+                1);
 
-        startActivity(i);
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Intent i = new Intent(MainActivity.this, FeedActivity.class);
+
+                    startActivity(i);
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your SMSs", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }

953d6dc3015b6637dc2d32f143d010474900c17b
noosrath <noosrat.hossain@gmail.com>
2019-04-04 14:59:21 +0200

    design changes

--- app/src/main/java/com/example/noosrat/budgettracker/SettingsActivity.java
+++ app/src/main/java/com/example/noosrat/budgettracker/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.noosrat.budgettracker;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -11,6 +12,9 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        TextView title = findViewById(R.id.balance_title);
+
+        title.setText("Preferences");
         getSupportActionBar().hide();
     }
 }
